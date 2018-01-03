119739666a5a553376aee51a45109314f618a951
Matt Oakley <matt.oakley@hashbanghq.com>
2018-01-03 13:52:34 +0000

    move sitemap generation into the middleman sitemap pipeline so that it can be viewed during development

--- lib/middleman-seo_sitemap/extension.rb
+++ lib/middleman-seo_sitemap/extension.rb
@@ -9,9 +9,11 @@ module Middleman
       option :priority, 0.5, 'Default priority for pages'
       option :ping_search_engines, true, 'Automatically ping sitemap to search engines?'
 
-      def after_build(builder)
+      def manipulate_resource_list(resources)
+        tmp_path = File.expand_path '../../../tmp/sitemap/sitemap.xml'
+
         SitemapGenerator::Sitemap.default_host = options.default_host
-        SitemapGenerator::Sitemap.public_path = app.config[:build_dir]
+        SitemapGenerator::Sitemap.public_path = '../../../tmp/sitemap'
         SitemapGenerator::Sitemap.include_root = false
         SitemapGenerator::Sitemap.compress = false
 
@@ -25,6 +27,14 @@ module Middleman
 
         SitemapGenerator::Sitemap.finalize!
 
+        sitemap = Middleman::Sitemap::Resource.new app.sitemap, 'sitemap.xml', tmp_path
+
+        logger.info '== middleman-seo_sitemap: sitemap.xml added to resources =='
+
+        resources << sitemap
+      end
+
+      def after_build(builder)
         SitemapGenerator::Sitemap.ping_search_engines if options.ping_search_engines
       end
     end
